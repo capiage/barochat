3d83414320acdae1db88d15cabb71561ebd7b831
AlexTechnologia <alexmusial551@gmail.com>
2026-05-13 21:31:26 +0000

    firebase fix

--- app.js
+++ app.js
@@ -271,7 +271,7 @@ window.nexusApp = () => ({
         try {
             this.supabase = createClient(
                 'https://tzdwxrdkqcntskwdvkfl.supabase.co', 
-                'sb_publishable_eX_AVyk3lqc-7rMEtjj1ow_i4aOLCk4'
+                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6ZHd4cmRrcWNudHNrd2R2a2ZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2ODUzNjksImV4cCI6MjA5NDI2MTM2OX0.MpMEqeXXgAGuOwu9fCgSYMmWsP4JHze2s2DerNrUBu4'
             );
 
             const savedSession = localStorage.getItem('lebarochat_session_v4');
